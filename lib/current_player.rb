35bd47e0fedcb605537808caa616da33222ad767
Lisa Webb <lwebbnyc@outlook.com>
2019-02-09 23:43:03 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,9 +10,9 @@ end
 
 
 def current_player(board)
-  if turn_count(board) % 2 ==0 
+  if turn_count(board) % 2 ==1 
     "X"
-  elseif turn_count(board) % 2 == 1  
+  elseif turn_count(board) % 2 == 0  
     "O"
 end
 end
