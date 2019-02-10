f358e5be8b7d77b3614babb4a81d53e4934d5bf1
Lisa Webb <lwebbnyc@outlook.com>
2019-02-10 00:10:39 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,12 +12,5 @@ end
 def current_player(board)
   turns = turn_count(board)
  if turns % 2 == 0 ? "X" : "O"
-    return "X"
-  else
-    return "O"
   end
 end
-
-score= 50
-result = score > 40 ? 'Pass' : 'Fail'
-puts result
